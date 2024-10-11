8748112c562385f59ee043e70789d9218ca67eda
bulskov <bulskov@ruc.dk>
2024-10-11 12:03:51 +0200

    make your own linq

--- 3_Linq/LinqProgram.cs
+++ 3_Linq/LinqProgram.cs
@@ -1,4 +1,7 @@
 ﻿using System.Linq;
+using System.Runtime.InteropServices;
+
+
 
 
 var query = Data.GetPersonData();
@@ -9,7 +12,31 @@ var q2 = query.Join(
     c => c.Id,
     (p, c) => new { p.Name, CourseName = c.Name, p.Age });
 
-foreach(var e in q2)
+var data = q2.ToList();
+
+foreach(var e in query.Select((x, i) => i))
 {
     Console.WriteLine(e);
+}
+
+
+static class LinqExt {
+    public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> fn)
+    {
+        foreach (var e in source)
+        {
+            if (fn(e))
+            {
+                yield return e;
+            }
+        }
+    }
+
+    public static IEnumerable<U> MySelect<T,U>(this IEnumerable<T> source, Func<T,U> fn)
+    {
+        foreach(var e in source)
+        {
+            yield return fn(e);
+        }
+    }
 }
