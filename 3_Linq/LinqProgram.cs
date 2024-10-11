0dd25d06eeb8c7eec7403313889eb78113b47a63
bulskov <bulskov@ruc.dk>
2024-10-11 11:16:16 +0200

    starting linq

--- 3_Linq/LinqProgram.cs
+++ 3_Linq/LinqProgram.cs
@@ -1,1 +1,15 @@
 ﻿using System.Linq;
+
+
+var query = Data.GetPersonData();
+
+var q2 = query.Join(
+    Data.GetCourses(),
+    p => p.CourseId,
+    c => c.Id,
+    (p, c) => new { p.Name, CourseName = c.Name, p.Age });
+
+foreach(var e in q2)
+{
+    Console.WriteLine(e);
+}
